911df37be0251098723a5f82c6f648cf099e68bd
Jeremy German <jeremy.german@gmail.com>
2021-12-01 08:56:59 -0500

    Day 1

---
+++ src/jgerman/advent_of_code_2021.clj
@@ -0,0 +1,46 @@
+(ns jgerman.advent-of-code-2021
+  (:gen-class)
+  (:require [clojure.java.io :as io]))
+
+(defn resource->edn [resource]
+  (-> resource
+      io/resource
+      slurp
+      read-string))
+
+(defn map-changes
+  "Given a collection of depths in integers return a collection containing true
+  for the pairs that increase and false otherwise."
+  [depths]
+  (map (fn [x y]
+         (< x y)) depths (rest depths)))
+
+(defn map-sliding-window [depths window-size]
+  (loop [window (take window-size depths)
+         remaining (drop 1 depths)
+         acc []]
+    (if (< (count window) window-size)
+      acc
+      (recur (take window-size remaining)
+             (drop 1 remaining)
+             (conj acc (apply + window))))))
+
+(defn count-increases [depth-changes]
+  (count (filter identity depth-changes)))
+
+(comment
+  (def sample-depths [199 200 208 210 200 207 240 269 260 263])
+  (count-increases (map-changes sample-depths))
+  (count-increases (map-changes (resource->edn "day1_input1.edn")))
+  (map-sliding-window sample-depths 3)
+  (count-increases (map-changes (map-sliding-window sample-depths 3)))
+  (-> "day1_input1.edn"
+      resource->edn
+      map-changes
+      count-increases)
+  (-> "day1_input1.edn"
+      resource->edn
+      (map-sliding-window 3)
+      map-changes
+      count-increases)
+  ,)
---
+++ test/jgerman/advent_of_code_2021_test.clj
@@ -0,0 +1,7 @@
+(ns jgerman.advent-of-code-2021-test
+  (:require [clojure.test :refer :all]
+            [jgerman.advent-of-code-2021 :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
