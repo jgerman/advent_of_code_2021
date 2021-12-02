c4d61f6a22230209cf2af3ad7c045d269575e2d8
Jeremy German <jeremy.german@gmail.com>
2021-12-02 08:55:14 -0500

    Day 2 complete.

--- src/jgerman/advent_of_code_2021.clj
+++ src/jgerman/advent_of_code_2021.clj
@@ -1,11 +1,24 @@
 (ns jgerman.advent-of-code-2021
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
 
 (defn resource->edn [resource]
   (-> resource
       io/resource
       slurp
       read-string))
+
+(defn resource->lines [resource]
+  (-> resource
+      io/resource
+      slurp
+      str/split-lines))
+
+(defn resource->day2-input [resource]
+  (map (fn [line]
+         (let [[direction magnitude] (str/split line #" ")]
+           [(keyword direction) (Integer/parseInt magnitude)]))
+       (resource->lines resource)))
 
 (defn map-changes
   "Given a collection of depths in integers return a collection containing true
@@ -27,6 +40,26 @@
 (defn count-increases [depth-changes]
   (count (filter identity depth-changes)))
 
+(defn apply-instruction [{:keys [aim] :as location}
+                         [direction magnitude]]
+  (case direction
+    :forward (-> location
+                 (update :horizontal + magnitude)
+                 (update :vertical + (* aim magnitude)))
+    :down (update location :aim + magnitude)
+    :up (update location :aim - magnitude)))
+
+(defn pilot [directions]
+  (reduce (fn [location instruction]
+            (apply-instruction location instruction))
+          {:horizontal 0
+           :vertical 0
+           :aim 0}
+          directions))
+
+(defn location->day2-solution [{:keys [horizontal vertical]}]
+  (* horizontal vertical))
+
 (comment
   (def sample-depths [199 200 208 210 200 207 240 269 260 263])
   (count-increases (map-changes sample-depths))
@@ -42,4 +75,12 @@
       (map-sliding-window 3)
       map-changes
       count-increases)
+  (resource->day2-input "day_2_input.txt")
+  (apply-instruction {:horizontal 0 :vertical 0} [:up 10])
+  (pilot (resource->day2-input "day_2_input.txt"))
+  (pilot (resource->day2-input "day_2_sample.txt"))
+  (-> "day_2_input.txt"
+      resource->day2-input
+      pilot
+      location->day2-solution)
   ,)
