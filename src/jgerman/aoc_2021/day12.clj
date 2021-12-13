6caf0f065bfe9ad8fb6af563ad4cf9c7e40b8254
Jeremy German <jeremy.german@gmail.com>
2021-12-12 19:34:48 -0500

    Tasks 1 and 2 working but need to factor out the dumb way I did task 1

--- src/jgerman/aoc_2021/day12.clj
+++ src/jgerman/aoc_2021/day12.clj
@@ -21,11 +21,6 @@
 (defn big-cave? [n]
   (every? #(Character/isUpperCase %) n))
 
-(defn exit-path? [ns]
-  (let [[s e] ((juxt first last) ns)]
-    (and (= "start" s)
-         (= "end" e))))
-
 (defn get-children [g current-path node]
   (let [children (get g node)]
     (filter (fn [n]
@@ -36,14 +31,46 @@
   ([g]
    (find-paths g [] [] "start"))
   ([g paths current-path node]
-   #_(tap> {:node node
-            :current-path current-path
-            :paths paths})
    (let [children (get-children g current-path node)
          current-path (conj current-path node)]
      (cond
        (= node "end") (conj paths current-path)
        :else (mapcat (partial find-paths g paths current-path) children)))))
+
+(defn used-twice? [p]
+  (not (every? #(= 1 %) (vals (frequencies (filter #(not (big-cave? %)) p))))))
+
+(defn can-add? [current-path node]
+  (when (= current-path ["start" "b" "d" "b" "A"])
+    (tap> {:current-path current-path
+           :node node
+           :not-used-twice? (not (used-twice? current-path))
+           :already-there? (some #{node} current-path)}))
+  (cond
+    (big-cave? node) true
+    (and (= node "start")
+         (empty? current-path)) true ;; I'm sure I could do this better
+    (= node "start") false
+    (= node "end") true ;; this is always ignored actually just makes path-search easier for now
+    (not (used-twice? current-path)) true
+    (some #{node} current-path) false
+    :else true))
+
+(defn path-search
+  ([g]
+   (path-search g [] [] "start"))
+  ([g paths current-path node]
+   (let [add? (can-add? current-path node)
+         children (get g node)]
+     (when (= current-path ["start" "b" "d" "b" "A"])
+       (tap> {:paths paths
+              :current-path current-path
+              :node node
+              :add? add?}))
+     (cond
+       (= node "end") (conj paths (conj current-path node)) ;; not striclty necessary to add the end
+       (not add?) paths
+       :else (mapcat (partial path-search g paths (conj current-path node)) children)))))
 
 (defn task-1 [resource]
   (-> resource
@@ -51,12 +78,23 @@
       find-paths
       count))
 
+(defn task-2 [resource]
+  (-> resource
+      resource->input
+      path-search))
+
 (comment
   (def sample (resource->input "day12_sample1.txt"))
-  (count (find-paths sample))
+
   (= 10 (task-1 "day12_sample1.txt"))
   (= 19 (task-1 "day12_sample2.txt"))
   (= 226 (task-1 "day12_sample3.txt"))
-  (task-1 "day12_input.txt")
+  (= 4775 (task-1 "day12_input.txt"))
+
+
+  (= 36 (count (task-2 "day12_sample1.txt")))
+  (= 103 (count (task-2 "day12_sample2.txt")))
+  (= 3509 (count (task-2 "day12_sample3.txt")))
+  (= 152480 (count (task-2 "day12_input.txt")))
   ;;marker
   )
