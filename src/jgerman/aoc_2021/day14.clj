511fd809b7d71ab4ad724875dcac96d3917efd95
Jeremy German <jeremy.german@gmail.com>
2021-12-14 19:14:34 -0500

    Both Day 14 tasks complete.

--- src/jgerman/aoc_2021/day14.clj
+++ src/jgerman/aoc_2021/day14.clj
@@ -1,8 +1,6 @@
 (ns jgerman.aoc-2021.day14
   (:require [jgerman.aoc-2021.utils :as utils]
-            [clojure.string :as str]
-            [clojure.set :as set]))
-
+            [clojure.string :as str]))
 
 (defn input-string->pairs [s]
   (loop [in s
@@ -16,7 +14,8 @@
 (defn build-rules [rules]
   (reduce merge {} (map (fn [r]
                           (let [[match replace] (str/split r #" -> ")]
-                            {[(first match) (second match)] (first replace)})) rules)))
+                            {[(first match) (second match)] [[(first match) (first replace)]
+                                                             [(first replace) (second match)]]})) rules)))
 
 (defn build-input [[input-string rules]]
   {:input-string input-string
@@ -25,42 +24,39 @@
 (defn resource->input [resource]
   (-> resource utils/resource->text (str/split #"\n\n") build-input))
 
-(defn match [rules pair]
-  (if-let [m (get rules pair)]
-    [[(first pair) m] [m (second pair)]]
-    [pair]))
+(defn calc [state]
+  (let [coll (sort
+              (vals (reduce (fn [acc [[ele _] cnt]]
+                              (update acc ele (fn [v] ((fnil + 0) v cnt))))
+                            {}
+                            (seq state))))]
+    (- (last coll) (first coll))))
 
-(defn fire-rules [rules pairs]
-  (loop [acc []
-         input pairs]
-    (let [curr (first input)]
-      (if (nil? curr)
-        acc
-        (recur (into acc (match rules curr)) (rest input))))))
+(defn step [rules state]
+  (reduce (fn [acc [pair cnt]]
+            (if-let [match (get rules pair)]
+              (-> acc
+                  (update (first match) (fn [v] ((fnil + 0) v cnt)))
+                  (update (second match) (fn [v] ((fnil + 0) v cnt))))
+              (assoc acc pair cnt)))
+          {}
+          (seq state)))
 
-;; this could probably be a reduce for a lazy seq
-(defn do-steps [rules pairs steps]
-  (loop [acc pairs
-         step 0]
-    (cond
-      (= step steps) acc
-      :else (recur (fire-rules rules acc) (inc step)))))
+(defn steps [rules state n]
+  (loop [acc state
+         s 0]
+    (if (= s n)
+      acc
+      (recur (step rules acc) (inc s)))))
 
-(defn task-1 [resource steps]
+(defn task-1 [resource n]
   (let [input (resource->input resource)
         pairs (input-string->pairs (:input-string input))
-        final (map first (do-steps (:rules input) pairs steps))
-        freqs (frequencies final)]
-    (- (apply max (vals freqs))
-       (apply min (vals freqs)))))
+        state (reduce (fn [m k] (assoc m k 1)) {} pairs)]
+    (calc (steps (:rules input) state n))))
 
 (comment
-  (resource->input "day14_sample.txt")
-  (input-string->pairs (:input-string (resource->input "day14_sample.txt")))
-
-  (task-1 "day14_sample.txt" 10)
-
   (= 3259 (task-1 "day14_input.txt" 10))
-  (= 1234 (task-1 "day14_input.txt" 40))
+  (= 3459174981021 (task-1 "day14_input.txt" 40))
   ;; marker
   )
